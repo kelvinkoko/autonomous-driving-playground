18444f55744e1eac0ad34e0d364ed995b6b21d50
kelvinko <kelvinkoko@gmail.com>
2023-08-12 00:10:33 +0900

    Init

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: "Playground for Autonomous Driving exploration",
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
+};
+
+module.exports = config;
