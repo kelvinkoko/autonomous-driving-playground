c89a14210ff123333cfc202c6ac1cf6cbec2376f
kelvinko <kelvinkoko@gmail.com>
2023-08-13 01:00:19 +0900

    Add cannon es

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as CANNON from "cannon-es";
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
@@ -12,18 +13,45 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
 camera.position.z = 5;
+
+const world = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.82, 0)
+});
+const radius = 1; // m
+const sphereBody = new CANNON.Body({
+  mass: 5, // kg
+  shape: new CANNON.Sphere(radius)
+});
+sphereBody.position.set(0, 10, 0); // m
+world.addBody(sphereBody);
+const groundBody = new CANNON.Body({
+  type: CANNON.Body.STATIC,
+  shape: new CANNON.Plane()
+});
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0); // make it face up
+world.addBody(groundBody);
+
+const geometry = new THREE.SphereGeometry(radius);
+const material = new THREE.MeshNormalMaterial();
+const sphereMesh = new THREE.Mesh(geometry, material);
+scene.add(sphereMesh);
 
 function animate() {
   requestAnimationFrame(animate);
+  world.fixedStep();
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  sphereMesh.position.set(
+    sphereBody.position.x,
+    sphereBody.position.y,
+    sphereBody.position.z
+  );
+  sphereMesh.quaternion.set(
+    sphereBody.quaternion.x,
+    sphereBody.quaternion.y,
+    sphereBody.quaternion.z,
+    sphereBody.quaternion.w
+  );
 
   renderer.render(scene, camera);
 }
