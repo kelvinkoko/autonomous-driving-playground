9760629a90fc7a7e7fdd250402ca95ee753e5ecc
kelvinko <kelvinkoko@gmail.com>
2023-09-04 22:21:53 +0900

    Extract glass effect css

--- src/Application/Ui/VehicleStateDisplay.tsx
+++ src/Application/Ui/VehicleStateDisplay.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react";
 import * as React from "react";
 import { WorldStore } from "../Store/WordStore";
+import glassPanelStyles from "./GlassPanels.css";
 import styles from "./VehicleStateDisplay.css";
 
 interface Props {
@@ -10,7 +11,7 @@ interface Props {
 const VehicleStateDisplay = observer(({ worldStore }: Props) => {
   const speedInKmPerHour = worldStore.carState.speedMS * (3600 / 1000);
   return (
-    <div className={styles.speed}>
+    <div className={`${glassPanelStyles.grayPanel} ${styles.speed}`}>
       <div></div>
       <div className={styles.speedValue}>{Math.round(speedInKmPerHour)}</div>
       <div className={styles.speedUnit}>{"km/h"}</div>
