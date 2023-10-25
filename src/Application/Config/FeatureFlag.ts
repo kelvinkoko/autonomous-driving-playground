d2e61da12b5cec56d5daccaab60c361a6924d6e8
kelvinko <kelvinkoko@gmail.com>
2023-10-26 01:08:01 +0900

    Override feature flag from query parameter

--- src/Application/Config/FeatureFlag.ts
+++ src/Application/Config/FeatureFlag.ts
@@ -1,1 +1,23 @@
-export const ENABLE_CODE_EDITOR = false;
+export const ENABLE_CODE_EDITOR = isEnableCodeEditor();
+
+function isEnableCodeEditor(): boolean {
+  const queryInput = getBooleanQueryParam(
+    "ENABLE_CODE_EDITOR",
+    window.location.search
+  );
+  if (queryInput === undefined) {
+    return false;
+  }
+  return queryInput;
+}
+
+function getBooleanQueryParam(paramName: string, queryString: string) {
+  const urlParams = new URLSearchParams(queryString);
+  const paramValue = urlParams.get(paramName);
+
+  if (paramValue === null) {
+    return undefined;
+  }
+
+  return paramValue === "true";
+}
