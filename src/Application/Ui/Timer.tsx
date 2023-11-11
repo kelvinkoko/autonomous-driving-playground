6f11b540be4da8f519379dcf5eb0c45d5821e335
kelvinko <kelvinkoko@gmail.com>
2023-11-12 00:51:08 +0900

    Implementing timer

---
+++ src/Application/Ui/Timer.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { useEffect, useState } from "react";
+import styles from "./Timer.css";
+
+const Timer = () => {
+  const [milliseconds, setMilliseconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMilliseconds(prevMilliseconds => prevMilliseconds + 10);
+    }, 10);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const minutes = Math.floor(milliseconds / 60000);
+  const seconds = ((milliseconds % 60000) / 1000).toFixed(2);
+  const minuteString = String(minutes).padStart(2, "0");
+  const secondString = String(seconds).padStart(5, "0");
+  return (
+    <div className={styles.time}>
+      <p>{`${minuteString}:${secondString}`}</p>
+    </div>
+  );
+};
+
+export default Timer;
